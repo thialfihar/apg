ab3c9413f64d13e33e87ae8839b5332687d0ec98
Dominik Schürmann <dominik@dominikschuermann.de>
2014-03-30 20:53:16 +0200

    Decrypt mostly works again

--- OpenPGP-Keychain/src/main/java/org/sufficientlysecure/keychain/service/ApgIntentService.java
+++ OpenPGP-Keychain/src/main/java/org/sufficientlysecure/keychain/service/ApgIntentService.java
@@ -454,29 +454,30 @@ public class ApgIntentService extends IntentService implements Progressable, Key
 
                         break;
 
-                    case TARGET_STREAM: /* decrypting stream from content uri */
-                        Uri providerUri = (Uri) data.getParcelable(ENCRYPT_PROVIDER_URI);
-
-                        // InputStream
-                        InputStream in = getContentResolver().openInputStream(providerUri);
-                        inLength = PgpHelper.getLengthOfStream(in);
-                        inputData = new InputData(in, inLength);
-
-                        // OutputStream
-                        try {
-                            while (true) {
-                                streamFilename = PgpHelper.generateRandomFilename(32);
-                                if (streamFilename == null) {
-                                    throw new PgpGeneralException("couldn't generate random file name");
-                                }
-                                openFileInput(streamFilename).close();
-                            }
-                        } catch (FileNotFoundException e) {
-                            // found a name that isn't used yet
-                        }
-                        outStream = openFileOutput(streamFilename, Context.MODE_PRIVATE);
-
-                        break;
+                    // TODO: not used, maybe contains code useful for new decrypt method for files?
+//                    case TARGET_STREAM: /* decrypting stream from content uri */
+//                        Uri providerUri = (Uri) data.getParcelable(ENCRYPT_PROVIDER_URI);
+//
+//                        // InputStream
+//                        InputStream in = getContentResolver().openInputStream(providerUri);
+//                        inLength = PgpHelper.getLengthOfStream(in);
+//                        inputData = new InputData(in, inLength);
+//
+//                        // OutputStream
+//                        try {
+//                            while (true) {
+//                                streamFilename = PgpHelper.generateRandomFilename(32);
+//                                if (streamFilename == null) {
+//                                    throw new PgpGeneralException("couldn't generate random file name");
+//                                }
+//                                openFileInput(streamFilename).close();
+//                            }
+//                        } catch (FileNotFoundException e) {
+//                            // found a name that isn't used yet
+//                        }
+//                        outStream = openFileOutput(streamFilename, Context.MODE_PRIVATE);
+//
+//                        break;
 
                     default:
                         throw new PgpGeneralException("No target choosen!");
@@ -513,11 +514,11 @@ public class ApgIntentService extends IntentService implements Progressable, Key
                         // nothing, file was written, just send okay and verification bundle
 
                         break;
-                    case TARGET_STREAM:
-                        String uri = DataStream.buildDataStreamUri(streamFilename).toString();
-                        resultData.putString(RESULT_URI, uri);
-
-                        break;
+//                    case TARGET_STREAM:
+//                        String uri = DataStream.buildDataStreamUri(streamFilename).toString();
+//                        resultData.putString(RESULT_URI, uri);
+//
+//                        break;
                 }
 
                 OtherHelper.logDebugBundle(resultData, "resultData");
