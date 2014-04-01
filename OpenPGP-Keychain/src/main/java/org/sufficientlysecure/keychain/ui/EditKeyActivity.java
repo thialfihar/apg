open-keychain/open-keychain

fix certify keys without sign flag

OpenPGP-Keychain/src/main/java/org/sufficientlysecure/keychain/ui/EditKeyActivity.java
@@ -256,7 +256,7 @@ public class EditKeyActivity extends ActionBarActivity {
             // get master key id using row id
             long masterKeyId = ProviderHelper.getSecretMasterKeyId(this, keyRingRowId);
 
-            masterCanSign = ProviderHelper.getSecretMasterKeyCanSign(this, keyRingRowId);
+            masterCanSign = ProviderHelper.getSecretMasterKeyCanCertify(this, keyRingRowId);
             finallyEdit(masterKeyId, masterCanSign);
         }
     }

thialfihar/apg

OpenPGP-Keychain/src/main/java/org/sufficientlysecure/keychain/ui/EditKeyActivity.java
@@ -268,8 +268,8 @@ public class EditKeyActivity extends ActionBarActivity {
             KeyRing keyRing = mProvider.getKeyRing(mDataUri);
             Key masterKey = keyRing.getMasterKey();
             long masterKeyId = masterKey.getKeyId();
-            mMasterCanSign = masterKey.isSigningKey();
 
+            mMasterCanSign = ProviderHelper.getSecretMasterKeyCanCertify(this, keyRingRowId);
             finallyEdit(masterKeyId, mMasterCanSign);
         }
     }